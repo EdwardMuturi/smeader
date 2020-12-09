1d97cb9b991a8e5f28dc455473c5971f56a90cc2
EdwardMuturi <kakaturi300@gmail.com>
2020-12-09 12:34:35 +0300

    move sms reading & extracting logic to smsView model, activity sends start signal to viewmodel and receives list of messages

--- app/src/main/java/com/mementoguy/smeader/MainActivity.kt
+++ app/src/main/java/com/mementoguy/smeader/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.MenuItem
 import android.widget.ArrayAdapter
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     val messageList = ArrayList<String>()
 
     lateinit var arrayAdapter: ArrayAdapter<String>
+    lateinit var smsViewModel:  SmsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,64 +30,8 @@ class MainActivity : AppCompatActivity() {
         arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, messageList)
         list.adapter = arrayAdapter
 
+        smsViewModel= ViewModelProvider(this).get(SmsViewModel::class.java)
         requestSmsPermission()
-
-        sendMpesaReceiptToServer(messageList)
-    }
-
-    private fun sendMpesaReceiptToServer(messageList: ArrayList<String>) {
-        messageList.forEach{ message ->
-            val fieldsArray = message.split(".")
-            fieldsArray.extractMpesaFields()
-        }
-    }
-
-    fun List<String>.extractMpesaFields() {
-
-        var mpesaReceipt: String? = null
-        var date: String? = null
-        var time: String? = null
-        var amount: String? = null
-        var am_pm: String? = null
-        var customer: String? = null
-        var accountNumber: String? = null
-
-        this.forEachIndexed { index, message ->
-
-            when (index) {
-                0 -> {
-                        mpesaReceipt = message.extractMpesaField().first()
-                }
-
-                1 -> {
-                    val dateArray = message.extractMpesaField()
-
-                    time = dateArray[4]
-                    am_pm = dateArray[5]
-                    date = "${dateArray[2]} $time $am_pm"
-                    amount = dateArray[6].removePrefix("Ksh")
-
-                }
-
-                2 -> {
-                    val customerArray= message.extractMpesaField()
-                    customer = "+${customerArray.find { it.startsWith("254", true) }}"
-                }
-
-                3 -> {
-                    val accountArray = message.extractMpesaField()
-                    accountNumber = "${accountArray[4]} ${accountArray[5]}"
-                }
-                4->{ val paymentFields= mapOf("transactionReceipt" to mpesaReceipt, "paidOn" to date, "amount" to amount, "accountNumber" to accountNumber, "customer" to customer)
-                    Log.e("Mpesa Field Data $index", paymentFields.entries.toString()) }
-            }
-
-        }
-
-    }
-
-    fun String.extractMpesaField(): List<String> {
-        return this.split(" ")
     }
 
     override fun onStart() {
@@ -107,57 +53,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun requestSmsPermission() {
-        if (ContextCompat.checkSelfPermission(
-                baseContext,
-                "android.permission.READ_SMS"
-            ) == PackageManager.PERMISSION_GRANTED
-        )
-            readSms("MPESA")
-        else {
-
+        if (ContextCompat.checkSelfPermission(baseContext, "android.permission.READ_SMS") == PackageManager.PERMISSION_GRANTED) {
+            messageList.addAll(smsViewModel.readSms(contentResolver, "MPESA"))
+            arrayAdapter.addAll(messageList)
+        } else {
             val REQUEST_CODE_READ_SMS = 123
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf("android.permission.READ_SMS"),
-                REQUEST_CODE_READ_SMS
-            )
+            ActivityCompat.requestPermissions(this, arrayOf("android.permission.READ_SMS"), REQUEST_CODE_READ_SMS)
         }
-    }
-
-    private fun readSms(senderId: String) {
-
-        val smsColumns = arrayOf(Telephony.TextBasedSmsColumns.BODY, Telephony.TextBasedSmsColumns.ADDRESS)
-        val smsSelection = "${Telephony.TextBasedSmsColumns.ADDRESS} =?"
-        val smsArgs = arrayOf(senderId)
-        val smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), smsColumns, smsSelection, smsArgs, null)
-        val indexBody = smsInboxCursor?.getColumnIndex("body")
-
-        if (!smsInboxCursor!!.moveToFirst()) return
-        arrayAdapter.clear()
-
-        do {
-            val smsMpesaSuccess = filterMpesaSuccessSms(smsInboxCursor.getString(indexBody!!))
-            if (smsMpesaSuccess.isNotBlank())
-                arrayAdapter.add(smsMpesaSuccess)
-
-        } while (smsInboxCursor.moveToNext())
     }
 
     fun updateList(smsMessage: String) {
         arrayAdapter.insert(smsMessage, 0)
         arrayAdapter.notifyDataSetChanged()
     }
-
-    fun filterMpesaSuccessSms(smsBody: String): String {
-        val smsFirstLineSubstring = smsBody.split(".").first()
-        var smsMpesaSuccess = ""
-
-        if (smsFirstLineSubstring.contains("Confirmed"))
-            smsMpesaSuccess = smsBody
-
-        return smsMpesaSuccess
-    }
-
 
     companion object {
         lateinit var mainActivityInstance: MainActivity
--- app/src/main/java/com/mementoguy/smeader/SmsViewModel.kt
+++ app/src/main/java/com/mementoguy/smeader/SmsViewModel.kt
@@ -1,44 +1,101 @@
 package com.mementoguy.smeader
 
+import android.content.ContentResolver
 import android.net.Uri
 import android.provider.Telephony
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class SmsViewModel : ViewModel() {
 
-//    private fun readSms(senderId: String) {
-//
-//        val smsColumns = arrayOf(Telephony.TextBasedSmsColumns.BODY, Telephony.TextBasedSmsColumns.ADDRESS)
-//        val smsSelection = "${Telephony.TextBasedSmsColumns.ADDRESS} =?"
-//        val smsArgs = arrayOf(senderId)
-//
-//        val smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), smsColumns, smsSelection, smsArgs, null)
-//        val indexBody = smsInboxCursor?.getColumnIndex("body")
-//
-//        if (!smsInboxCursor!!.moveToFirst()) return
-//        arrayAdapter.clear()
-//
-//        do {
-//            val smsMpesaSuccess = filterMpesaSuccessSms(smsInboxCursor.getString(indexBody!!))
-//            if (smsMpesaSuccess.isNotBlank())
-//                arrayAdapter.add(smsMpesaSuccess)
-//
-//        } while (smsInboxCursor.moveToNext())
-//    }
-//
-//    fun updateList(smsMessage: String) {
-//        arrayAdapter.insert(smsMessage, 0)
-//        arrayAdapter.notifyDataSetChanged()
-//    }
-//
-//    fun filterMpesaSuccessSms(smsBody: String): String {
-////        each successfull transaction has 'Confirmed' keyword before the first fullstop `.`
-//        val smsFirstLineSubstring = smsBody.split(".").first()
-//        var smsMpesaSuccess = ""
-//
-//        if (smsFirstLineSubstring.contains("Confirmed"))
-//            smsMpesaSuccess = smsBody
-//
-//        return smsMpesaSuccess
-//    }
+    fun readSms(contentResolver: ContentResolver, senderId: String): List<String> {
+
+        val smsMessages : MutableList<String> = ArrayList()
+
+        val smsColumns = arrayOf(Telephony.TextBasedSmsColumns.BODY, Telephony.TextBasedSmsColumns.ADDRESS)
+        val smsSelection = "${Telephony.TextBasedSmsColumns.ADDRESS} =?"
+        val smsArgs = arrayOf(senderId)
+        val smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), smsColumns, smsSelection, smsArgs, null)
+        val indexBody = smsInboxCursor?.getColumnIndex("body")
+
+        if (!smsInboxCursor!!.moveToFirst())
+            smsMessages.clear()
+
+        do {
+            val smsMpesaSuccess = filterMpesaSuccessSms(smsInboxCursor.getString(indexBody!!))
+            if (smsMpesaSuccess.isNotBlank())
+                smsMessages.add(smsMpesaSuccess)
+
+        } while (smsInboxCursor.moveToNext())
+
+        sendMpesaReceiptToServer(smsMessages as ArrayList<String>)
+
+        return smsMessages
+    }
+
+    fun filterMpesaSuccessSms(smsBody: String): String {
+
+    val smsFirstLineSubstring = smsBody.split(".").first()
+        var smsMpesaSuccess = ""
+
+        if (smsFirstLineSubstring.contains("Confirmed"))
+            smsMpesaSuccess = smsBody
+
+        return smsMpesaSuccess
+    }
+
+    fun sendMpesaReceiptToServer(messageList: ArrayList<String>) {
+        messageList.forEach{ message ->
+            val fieldsArray = message.split(".")
+            fieldsArray.extractMpesaFields()
+        }
+    }
+
+    fun List<String>.extractMpesaFields() {
+
+        var mpesaReceipt: String? = null
+        var date: String? = null
+        var time: String? = null
+        var amount: String? = null
+        var am_pm: String? = null
+        var customer: String? = null
+        var accountNumber: String? = null
+
+        this.forEachIndexed { index, message ->
+
+            when (index) {
+                0 -> {
+                    mpesaReceipt = message.extractMpesaField().first()
+                }
+
+                1 -> {
+                    val dateArray = message.extractMpesaField()
+
+                    time = dateArray[4]
+                    am_pm = dateArray[5]
+                    date = "${dateArray[2]} $time $am_pm"
+                    amount = dateArray[6].removePrefix("Ksh")
+
+                }
+
+                2 -> {
+                    val customerArray= message.extractMpesaField()
+                    customer = "+${customerArray.find { it.startsWith("254", true) }}"
+                }
+
+                3 -> {
+                    val accountArray = message.extractMpesaField()
+                    accountNumber = "${accountArray[4]} ${accountArray[5]}"
+                }
+                4->{ val paymentFields= mapOf("transactionReceipt" to mpesaReceipt, "paidOn" to date, "amount" to amount, "accountNumber" to accountNumber, "customer" to customer)
+                    Log.e("Mpesa Fields Map", paymentFields.entries.toString()) }
+            }
+
+        }
+
+    }
+
+    fun String.extractMpesaField(): List<String> {
+        return this.split(" ")
+    }
 }
